c8f0506f4f2eac3d5711e93d10b2ea1455321c05
Arman-Kabir <armankabir8@gmail.com>
2023-09-27 17:10:50 +0600

    sidebar title modified

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Layout,Menu } from "antd";
+import { Layout, Menu } from "antd";
 import { useState } from "react";
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -9,8 +9,8 @@ import {
     PieChartOutlined,
     TeamOutlined,
     UserOutlined,
-  } from '@ant-design/icons';
-  
+} from '@ant-design/icons';
+
 import type { MenuProps } from 'antd';
 
 type MenuItem = Required<MenuProps>['items'][number];
@@ -20,33 +20,46 @@ function getItem(
     key: React.Key,
     icon?: React.ReactNode,
     children?: MenuItem[],
-  ): MenuItem {
+): MenuItem {
     return {
-      key,
-      icon,
-      children,
-      label,
+        key,
+        icon,
+        children,
+        label,
     } as MenuItem;
-  }
-  
-  const items: MenuItem[] = [
+}
+
+const items: MenuItem[] = [
     getItem('Option 1', '1', <PieChartOutlined />),
     getItem('Option 2', '2', <DesktopOutlined />),
     getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5'),
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
     ]),
     getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
     getItem('Files', '9', <FileOutlined />),
-  ];
+];
 
 
 const Sidebar = () => {
     const [collapsed, setCollapsed] = useState(false);
     return (
-        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-            <div className="demo-logo-vertical" />
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}
+            width={280}
+            style={{
+                overflow: "auto",
+                height: "100vh",
+                position: "sticky",
+                left: 0,
+                top: 0,
+                bottom: 0,
+            }}
+        >
+
+            <div style={{ color: "white", fontSize: "2rem", textAlign: "center", fontWeight: "bold", marginBottom: "1rem" }}>
+                PH-University
+            </div>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
         </Sider>
     )
