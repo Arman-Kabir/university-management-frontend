41401626e078be98eda4a7118f9903d858174786
Arman-Kabir <armankabir8@gmail.com>
2023-09-30 01:07:01 +0600

    retrieving logged-in user from localStorage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ import Form from '@/components/Forms/Form';
 import FormInput from '@/components/Forms/FormInput';
 import { SubmitHandler } from 'react-hook-form';
 import { useUserLoginMutation } from '@/redux/api/authApi';
-import { storeUserInfo } from '@/services/auth.service';
+import { getUserInfo, isLoggedIn, storeUserInfo } from '@/services/auth.service';
 
 type FormValues = {
   id: string;
@@ -16,6 +16,8 @@ type FormValues = {
 
 const LoginPage = () => {
   const [userLogin] = useUserLoginMutation();
+
+  console.log(isLoggedIn());
 
   const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
     try {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,24 @@
-import { setToLocalStorage } from "@/utils/local-storage"
+import { authKey } from "@/constants/storageKey";
+import { decodedToken } from "@/utils/jwt";
+import { getFromLocalStorage, setToLocalStorage } from "@/utils/local-storage"
 
 export const storeUserInfo = ({ accessToken }: { accessToken: string }) => {
+    return setToLocalStorage(authKey, accessToken as string)
+}
 
-    setToLocalStorage("accessToken",accessToken as string)
+export const getUserInfo = () => {
+    const authToken = getFromLocalStorage(authKey);
+    // console.log(authLocalStorageData);
+    if (authToken) {
+        const decodedData = decodedToken(authToken);
+        return decodedData;
+    }else{
+        return ""
+    }
+}
 
-
+export const isLoggedIn=()=>{
+    const authToken = getFromLocalStorage(authKey);
+    
+    return !!authToken
 }
