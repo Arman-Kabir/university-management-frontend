565d707f6f88fbf22cb0542805981efe7e703168
Arman-Kabir <armankabir8@gmail.com>
2023-09-30 10:27:45 +0600

    axios instance request

--- src/helpers/axios/axiosBaseQuery.ts
+++ src/helpers/axios/axiosBaseQuery.ts
@@ -1,7 +1,7 @@
 import { IMeta } from '@/types'
 import type { BaseQueryFn } from '@reduxjs/toolkit/query'
-import axios from 'axios'
 import type { AxiosRequestConfig, AxiosError } from 'axios'
+import { instance as axiosInstance } from './axiosInstance'
 
 export const axiosBaseQuery =
   (
@@ -12,24 +12,28 @@ export const axiosBaseQuery =
       method: AxiosRequestConfig['method']
       data?: AxiosRequestConfig['data']
       params?: AxiosRequestConfig['params']
-      meta?:IMeta
-      contentType?:string
+      meta?: IMeta
+      contentType?: string
     },
     unknown,
     unknown
   > =>
-  async ({ url, method, data, params }) => {
-    try {
-      const result = await axios({ url: baseUrl + url, method, data, params })
-      return { data: result.data }
-    } catch (axiosError) {
-      let err = axiosError as AxiosError
-      return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
+    async ({ url, method, data, params, contentType }) => {
+      try {
+        const result = await axiosInstance({
+          url: baseUrl + url, method, data, params, headers: {
+            contentType: contentType || "application/json"
+          }
+        })
+        return { data: result.data }
+      } catch (axiosError) {
+        let err = axiosError as AxiosError
+        return {
+          error: {
+            status: err.response?.status,
+            data: err.response?.data || err.message,
+          },
+        }
       }
     }
-  }
 
