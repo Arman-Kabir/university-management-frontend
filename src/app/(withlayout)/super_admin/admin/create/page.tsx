fb3a1f06d8575fbf9d6ab68a6ca7899935a61bad
Arman-Kabir <armankabir8@gmail.com>
2023-10-06 23:55:13 +0600

    Image uploader

--- src/app/(withlayout)/super_admin/admin/create/page.tsx
+++ src/app/(withlayout)/super_admin/admin/create/page.tsx
@@ -4,6 +4,7 @@ import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import FormSelectField from "@/components/Forms/FormSelectField";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb"
+import UploadImage from "@/components/ui/UploadImage";
 import { departmentOptions, genderOptions } from "@/constants/global";
 import { Button, Col, Row } from "antd"
 
@@ -35,6 +36,7 @@ const CreateAdminPage = () => {
 
             <div>
                 <Form submitHandler={onSubmit}>
+
                     <div style={{ border: "1px solid #d9d9d9", borderRadius: "5px", padding: "15px", marginBottom: "10px" }}>
 
                         <p style={{
@@ -43,42 +45,102 @@ const CreateAdminPage = () => {
                         }}>Admin Information</p>
                         <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
                             <Col className="gutter-row" span={8} style={{
-                                 marginBottom: "10px"
+                                marginBottom: "10px"
                             }}>
                                 <FormInput type="text" name="admin.name.firstName" size="large" label="First Name"></FormInput>
                             </Col>
 
                             <Col className="gutter-row" span={8} style={{
-                                 marginBottom: "10px"
+                                marginBottom: "10px"
                             }}>
                                 <FormInput type="text" name="admin.name.middleName" size="large" label="Middle Name"></FormInput>
                             </Col>
 
                             <Col className="gutter-row" span={8} style={{
-                                 marginBottom: "10px"
+                                marginBottom: "10px"
                             }}>
                                 <FormInput type="text" name="admin.name.lastName" size="large" label="Last Name"></FormInput>
                             </Col>
 
                             <Col className="gutter-row" span={8} style={{
-                                 marginBottom: "10px"
+                                marginBottom: "10px"
                             }}>
                                 <FormInput type="password" name="password" size="large" label="Password"></FormInput>
                             </Col>
 
                             <Col className="gutter-row" span={8} style={{
-                                 marginBottom: "10px"
+                                marginBottom: "10px"
                             }}>
                                 <FormSelectField size="large" name="admin.gender" options={genderOptions} label="Gender" placeholder="Select"></FormSelectField>
                             </Col>
 
                             <Col className="gutter-row" span={8} style={{
-                                 marginBottom: "10px"
+                                marginBottom: "10px"
                             }}>
                                 <FormSelectField size="large" name="admin.managementDepartment" options={departmentOptions} label="Department" placeholder="Select"></FormSelectField>
                             </Col>
+
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <UploadImage>
+
+                                </UploadImage>
+                            </Col>
+
                         </Row>
                     </div>
+
+                    {/* basic info  */}
+                    <div style={{ border: "1px solid #d9d9d9", borderRadius: "5px", padding: "15px", marginBottom: "10px" }}>
+
+                        <p style={{
+                            fontSize: "18px",
+                            marginBottom: "10px"
+                        }}>Basic Information</p>
+
+                        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <FormInput type="email" name="admin.email" size="large" label="Email"></FormInput>
+                            </Col>
+
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <FormInput type="text" name="admin.contactNo" size="large" label="Contact Number"></FormInput>
+                            </Col>
+
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <FormInput type="text" name="admin.emergencyContactNo" size="large" label="Emergency Contact Number"></FormInput>
+                            </Col>
+
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <FormInput type="password" name="password" size="large" label="Password"></FormInput>
+                            </Col>
+
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <FormSelectField size="large" name="admin.gender" options={genderOptions} label="Gender" placeholder="Select"></FormSelectField>
+                            </Col>
+
+                            <Col className="gutter-row" span={8} style={{
+                                marginBottom: "10px"
+                            }}>
+                                <FormSelectField size="large" name="admin.managementDepartment" options={departmentOptions} label="Department" placeholder="Select"></FormSelectField>
+                            </Col>
+
+
+                        </Row>
+                    </div>
+
+
                     <Button htmlType="submit" type="primary">Create</Button>
                 </Form>
             </div>
