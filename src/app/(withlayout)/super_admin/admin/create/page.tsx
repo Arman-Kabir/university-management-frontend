0dd8d4b3d59482ac54243552df406cc6897528d0
Arman-Kabir <armankabir8@gmail.com>
2023-10-07 20:04:18 +0600

    yup schema done

--- src/app/(withlayout)/super_admin/admin/create/page.tsx
+++ src/app/(withlayout)/super_admin/admin/create/page.tsx
@@ -8,6 +8,8 @@ import FormTextArea from "@/components/Forms/FormTextArea";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb"
 import UploadImage from "@/components/ui/UploadImage";
 import { bGroupOptions, departmentOptions, genderOptions } from "@/constants/global";
+import { adminSchema } from "@/schemas/admin";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Button, Col, Row } from "antd"
 
 const CreateAdminPage = () => {
@@ -37,7 +39,7 @@ const CreateAdminPage = () => {
             <h1>Create Admin</h1>
 
             <div>
-                <Form submitHandler={onSubmit}>
+                <Form submitHandler={onSubmit} resolver={yupResolver(adminSchema)}>
 
                     <div style={{ border: "1px solid #d9d9d9", borderRadius: "5px", padding: "15px", marginBottom: "10px" }}>
 
